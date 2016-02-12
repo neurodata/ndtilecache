1c20983a9087d34b87c9bca8451b06cbca86f4f4
Kunal Lillaney <lillaney@jhu.edu>
2016-02-12 01:33:45 -0500

    [test] Benchmarks scripts and packages script.

---
+++ setup/packages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt-get update && apt-get upgrade -y
+
+#apt-get -y install nginx git bash-completion python-virtualenv python-virtualenvwrapper libxslt libxslt-devel hdf5-devel libmemcached-devel libjpeg-turbo-devel gcc-c++
+
+#pip install cython numpy django pytest posix_ipc boto3 nibabel networkx requests lxml h5py pylibmc pillow blosc django-registration django-registration-redux django-celery mysql-python libtiff
+
